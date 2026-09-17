49fdac26f995717de2cadd6faa237596280e7013
naterrie <naterrie@student.42lyon.fr>
2026-09-17 15:43:52 +0200

    removing flutter pre generated code

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Swift companion',
+      title: 'Swifty companion',
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
 
+
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink.shade50),
       ),
-      home: const MyHomePage(title: 'Swifty companion', ),
+      home: const MyHomePage(title: 'Swifty companion'),
     );
   }
 }
@@ -32,13 +33,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,24 +40,16 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        centerTitle: true,
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            const Text('Search for a student or staff information :'),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      )
     );
   }
 }
